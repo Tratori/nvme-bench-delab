c0a02944ada905ab1f6a221401c4c2036885b11d
Fabian Mahling <FabianMahling@web.de>
2024-01-16 13:20:34 +0100

    add script to extract benchmark results

---
+++ get_results.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import json
+
+# usage e.g.: python get_results.py results/mixed_read_write_results/ results.json
+configs = ["nx01", "gx05", "nx02", "gx03", "gx04", "gx01_raid1"]
+
+
+def main():
+    input_dir = sys.argv[1]
+    output_path = sys.argv[2]
+
+    results = {}
+    for config in configs:
+        benchmark_file = input_dir + f"{config}/benchmark"
+        if os.path.exists(benchmark_file):
+            with open(benchmark_file, "r") as fp:
+                benchmark = json.load(fp)
+                for key, value in benchmark.items():
+                    results[key] = value
+    with open(output_path, "w") as fp:
+        json.dump(results, fp)
+
+
+if __name__ == "__main__":
+    main()
