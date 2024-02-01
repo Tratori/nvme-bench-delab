7e6680dfda312719001471a69068c7ddd3c976ee
Fabian Mahling <FabianMahling@web.de>
2024-02-01 15:53:54 +0100

    use direct when writing to disk

--- write_to_disk.sh
+++ write_to_disk.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 GB=$1
 THREADS=$2
 SIZE_IN_BYTES=$(($GB * 1024 * 1024 * 1024))
 BYTES_PER_THREAD=$(($SIZE_IN_BYTES / $THREADS))
 seq 0 $BYTES_PER_THREAD $(($SIZE_IN_BYTES - 1)) |
-  parallel -k dd if=/dev/zero bs=64k iflag=fullblock,count_bytes count=$BYTES_PER_THREAD of=./write_{}
+  parallel -k dd if=/dev/zero bs=64k iflag=fullblock,count_bytes oflag=direct count=$BYTES_PER_THREAD of=./write_{}
 
 seq 0 $BYTES_PER_THREAD $(($SIZE_IN_BYTES - 1)) |
   parallel -k rm ./write_{}
